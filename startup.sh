233a684e8bce9e38f825e5befd33333aaf668fa7
Pahud Hsieh <pahudnet@gmail.com>
2017-05-21 17:03:32 +0800

    minor

--- startup.sh
+++ startup.sh
@@ -4,8 +4,12 @@ az=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone
 region=${az%%[a-z]}
 
 YourName=`aws --region ${region} ssm get-parameters --names ECSYourName --query 'Parameters[0].Value' --output text 2>/dev/null` 
+YourPassword=`aws --region ${region} ssm get-parameters --names ECSYourPassword --query 'Parameters[0].Value' --output text 2>/dev/null` 
 
 if [ -z ${YourName} ]; then 
+	YourName="##Undefinied##"
+fi
+if [ -z ${YourPassword} ]; then 
 	YourName="##Undefinied##"
 fi
 
@@ -16,7 +20,10 @@ cat > /srv/greeting.html <<EOF
 <title>EC2 Parameter Store demo</title>
 </head>
 <body>
+<p>
 <h1>Hi ${YourName}!</h1>
+<p>
+<code>Your password is ${YourPassword}!</code>
 EOF
 
 /usr/bin/caddy --conf /etc/Caddyfile --log stdout
